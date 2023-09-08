e8fa11b754a33f546829ec5d4eef238f1245f112
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-08 21:08:33 +0300

    feat: import headerDisplay in headerSide

--- src/headerSide/HeaderSide.tsx
+++ src/headerSide/HeaderSide.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './HeaderSide.module.scss';
 import HeaderAddBtn from '../headerAddBtn/HeaderAddBtn';
+import HeaderDisplay from '../headerDisplay/HeaderDisplay';
 
 const HeaderSide = () => {
   return (
     <div className={styles.side}>
       <HeaderAddBtn />
+      <HeaderDisplay number={10} />
     </div>
   );
 };
