506ba14b7513f9b72106cbbbe8bc9eff43ef65f0
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-08 19:41:25 +0300

    feat: `CardFooter` component

---
+++ src/cardFooter/CardFooter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CardFooter.module.scss'
+
+const CardFooter = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default CardFooter;
