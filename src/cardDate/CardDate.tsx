ec194531a8551c2693d5f7cc91c7a2f44b916804
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-08 19:11:34 +0300

    feat: `cardDate` component

---
+++ src/cardDate/CardDate.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './CardDate.module.scss';
+
+const CardDate = ({ date }: { date: string }) => {
+  return <p>{date}</p>;
+};
+
+export default CardDate;
