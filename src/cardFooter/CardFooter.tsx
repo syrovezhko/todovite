9061a9a6acccc8ce56d96211b7d0d780e63136a5
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-08 20:14:49 +0300

    feat: import CardUserImg in CardFooter

--- src/cardFooter/CardFooter.tsx
+++ src/cardFooter/CardFooter.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './CardFooter.module.scss';
 import CardBtnContainer from '../cardBtnContainer/CardBtnContainer';
+import CardUserImg from '../cardUserImg/CardUserImg';
 
 const CardFooter = () => {
   return (
     <div>
       <CardBtnContainer />
+      <CardUserImg />
     </div>
   );
 };
