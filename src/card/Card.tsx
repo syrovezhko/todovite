f620a58fedffc78ba1b71ad4fcd879fd23092fea
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-08 17:53:09 +0300

    feat: state to detect changes of `checked` prop

--- src/card/Card.tsx
+++ src/card/Card.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 import styles from './Card.module.scss'
 import { IData } from '../main/Main';
 
 const Card: FC<IData> = ({userId, id, title, completed}) => {
+  const [checked, setChecked] = useState(completed)
   return (
     <div>
       card
