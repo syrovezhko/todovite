e5f3a22083e08f9cac6094e60f5083dc3ef75efe
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-08 17:55:34 +0300

    feat: `import` of cardCHeckbox in Card

--- src/card/Card.tsx
+++ src/card/Card.tsx
@@ -1,13 +1,14 @@
 import React, { FC, useState } from 'react';
 import styles from './Card.module.scss'
+import CardCheckbox from '../cardCheckbox/CardCheckbox';
 import { IData } from '../main/Main';
 
 const Card: FC<IData> = ({userId, id, title, completed}) => {
   const [checked, setChecked] = useState(completed)
   return (
-    <div>
-      card
-    </div>
+      <CardCheckbox
+        completed={checked}
+        setChecked={setChecked}/>
   );
 };
 
