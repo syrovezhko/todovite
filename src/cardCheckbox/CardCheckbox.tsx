e710eedbb43db11fa94ebba8ad429efe3b146632
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-08 17:50:15 +0300

    feat: cardCheckbox component

---
+++ src/cardCheckbox/CardCheckbox.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+import { IData } from '../main/Main';
+
+interface ICheckbox extends IData {
+  setChecked: CallableFunction;
+}
+
+const CardCheckbox: FC<ICheckbox> = ({completed, setChecked}) => {
+  return (
+    <input
+      type="checkbox"
+      checked={completed}
+      onClick={()=>setChecked(!completed)}/>
+  );
+};
+
+export default CardCheckbox;
