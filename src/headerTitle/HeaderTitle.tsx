7b55cd17d8e5517c5e288b0b2b890bc1208834c7
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-08 20:34:18 +0300

    feat: headerTitle

---
+++ src/headerTitle/HeaderTitle.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HeaderTitle.module.scss'
+
+const HeaderTitle = () => {
+  return (
+    <h1 className={styles.title}>
+      Today
+    </h1>
+  );
+};
+
+export default HeaderTitle;
