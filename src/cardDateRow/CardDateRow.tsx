64fc25d71b09e53567d758dc1aaa9ccd37f5d671
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-08 19:15:39 +0300

    feat: randomDate function

--- src/cardDateRow/CardDateRow.tsx
+++ src/cardDateRow/CardDateRow.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 import styles from './CardDateRow.module.scss';
 
 const CardDateRow = () => {
+  const randomDate = (start: Date, end: Date) => {
+    const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    const options: Intl.DateTimeFormatOptions = {
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true
+    };
+    return date.toLocaleString('en-US', options);
+  };
   return (
     <div className={styles.row}>
     </div>
