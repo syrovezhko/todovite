d182835984e94dad4a2e67208485d095b31d016f
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-08 20:14:10 +0300

    feat: cardUserImg component

---
+++ src/cardUserImg/CardUserImg.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './CardUserImg.module.scss';
+import avatar from './../../public/Img.png';
+
+const CardUserImg = () => {
+  return <img src={avatar} alt="avatar" />;
+};
+
+export default CardUserImg;
