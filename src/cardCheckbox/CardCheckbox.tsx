4302fd1043c83eb353b660ef9fc77ffc510d92d9
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-10 17:34:28 +0300

    refactor: changed cardUserImg's path to svg

--- src/cardCheckbox/CardCheckbox.tsx
+++ src/cardCheckbox/CardCheckbox.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from 'react';
 import { IData } from '../main/Main';
+import styles from './CardCheckbox.module.scss';
 
 interface ICheckbox extends IData {
   setChecked: CallableFunction;
 }
 
 const CardCheckbox: FC<ICheckbox> = ({ completed, setChecked }) => {
-  return <input type="checkbox" checked={completed} onChange={() => setChecked(!completed)} />;
+  return (
+    <input
+      className={styles.check}
+      type="checkbox"
+      checked={completed}
+      onChange={() => setChecked(!completed)}
+    />
+  );
 };
 
 export default CardCheckbox;
