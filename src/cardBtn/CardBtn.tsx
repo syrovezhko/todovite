9b7ec4652c437d273176aa1d5db567dd82f23ffd
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-10 17:29:10 +0300

    feat: arrow img for secondary type of cardBtn

--- src/cardBtn/CardBtn.tsx
+++ src/cardBtn/CardBtn.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './CardBtn.module.scss';
+import img from './../../public/cardBtn.svg';
 
 const CardBtn = ({ type }: { type: string }) => {
   return (
       <p>{type === 'primary' ? 'Entity title' : `Front-end`}</p>
+      {Boolean(type === 'secondary') && <img src={img} alt="arrow" />}
     </button>
   );
 };
