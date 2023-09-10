27b7eda331396da76a92e0e1083ef5e5240f1915
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-09-10 13:24:36 +0300

    feat: transmit CardDate as a child to CardRow

--- src/card/Card.tsx
+++ src/card/Card.tsx
@@ -3,17 +3,24 @@ import styles from './Card.module.scss';
 import CardCheckbox from '../cardCheckbox/CardCheckbox';
 import { IData } from '../main/Main';
 import CardTitle from '../cardTitle/CardTitle';
-import CardDateRow from '../cardDateRow/CardDateRow';
+import CardRow from '../cardRow/CardRow';
 import CardText from '../cardText/CardText';
 import CardFooter from '../cardFooter/CardFooter';
+import CardDate from '../cardDate/CardDate';
 
 const Card: FC<IData> = ({ userId, id, title, completed }) => {
   const [checked, setChecked] = useState(completed);
+  const dates = () => (
+    <>
+      <CardDate />
+      <CardDate />
+    </>
+  );
   return (
-    <form>
+    <form className={styles.card}>
       <CardCheckbox completed={checked} setChecked={setChecked} />
       <CardTitle title={title} />
-      <CardDateRow />
+      <CardRow children={dates} type="date" />
       <CardText />
       <CardFooter />
     </form>
